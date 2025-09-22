5d08890bef224eec2b9ed925968ab0998c78bcc7
Mehyar <mehyarsalem44@gmail.com>
2025-09-21 21:51:56 -0400

    Update ThreeSum.java

--- Lab 2/ThreeSum.java
+++ Lab 2/ThreeSum.java
@@ -7,7 +7,7 @@ public class ThreeSum {
       for (int j = i+1; j < n; j++) 
         for (int k = j+1; k < n; k++) 
           if (a[i] + a[j] + a[k] == 0) 
-            StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+            System.out.println(a[i] + " " + a[j] + " " + a[k]);
   } 
 
   public static int count(int[] a) {
